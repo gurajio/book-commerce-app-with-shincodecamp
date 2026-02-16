fe66c4cdf6c10f06d338a23bf876880138b938cb
gurajio <gurajio0315@gmail.com>
2026-02-16 20:56:33 +0900

    Update UI and add login page

---
+++ app/lib/next-auth/options.ts
@@ -0,0 +1,27 @@
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github"
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+export const nextAuthOptions: NextAuthOptions = {
+    debug: false,
+    providers:[
+        GithubProvider({
+            // GITHUB_IDとSECRETがUndefinedの場合にclientIdなどに波線が出るから、「!」で回避
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!,
+        }),
+    ],
+    // prismaはインスタンス化したprismaを入れる
+    adapter: PrismaAdapter(prisma),
+    callbacks:{
+        session:({session, user}) => {
+            return{
+                ...session,
+                user:{
+                    ...session.user,
+                    id: user.id
+                },
+            };
+        }
+    }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,79 @@
-import Image from "next/image";
+// import Image from "next/image";
 
+// export default function Home() {
+//   return (
+//     <div>
+
+//     </div>
+//   );
+// }
+
+"use client";
+
+import Book from "./components/Book";
+
+// 疑似データ
+const books = [
+  {
+    id: 1,
+    title: "Book 1",
+    thumbnail: "/thumbnails/discord-clone-udemy.png",
+    price: 2980,
+    author: {
+      id: 1,
+      name: "Author 1",
+      description: "Author 1 description",
+      profile_icon: "https://source.unsplash.com/random/2",
+    },
+    content: "Content 1",
+    created_at: new Date().toString(),
+    updated_at: new Date().toString(),
+  },
+  {
+    id: 2,
+    title: "Book 2",
+    thumbnail: "/thumbnails/notion-udemy.png",
+    price: 1980,
+    author: {
+      id: 2,
+      name: "Author 2",
+      description: "Author 2 description",
+      profile_icon: "https://source.unsplash.com/random/3",
+    },
+    content: "Content 2",
+    created_at: new Date().toString(),
+    updated_at: new Date().toString(),
+  },
+  {
+    id: 3,
+    title: "Book 3",
+    price: 4980,
+    thumbnail: "/thumbnails/openai-chatapplication-udem.png",
+    author: {
+      id: 3,
+      name: "Author 3",
+      description: "Author 3 description",
+      profile_icon: "https://source.unsplash.com/random/4",
+    },
+    content: "Content 3",
+    created_at: new Date().toString(),
+    updated_at: new Date().toString(),
+  },
+  // 他の本のデータ...
+];
+
+// eslint-disable-next-line @next/next/no-async-client-component
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <>
+      <main className="flex flex-wrap justify-center items-center md:mt-32 mt-20">
+        <h2 className="text-center w-full font-bold text-3xl mb-2">
+          Book Commerce
+        </h2>
+        {books.map((book) => (
+          <Book key={book.id} book={book} />
+        ))}
       </main>
-    </div>
+    </>
   );
 }
