3f05c0bc6ac46613d4a0b65930745a1a327e8356
gurajio <gurajio0315@gmail.com>
2026-03-10 10:24:47 +0900

    solve error of purchases

--- app/api/purchases/[userid]/route.ts
+++ app/api/purchases/[userid]/route.ts
@@ -1,10 +1,11 @@
 import { prisma } from "@/app/lib/prisma"
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: Request,
-    {params}:{params:{userId: string}}
+export async function GET(
+    request: Request,
+    context: {params:Promise<{userId: string}>}
 ) {
-    const userId = params.userId
+    const {userId} = await context.params
     try{
         const purchases = await prisma.purchase.findMany({
             where: {userId: userId}
